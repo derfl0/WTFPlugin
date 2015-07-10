40711bc6bd0423ddfd7f8b2f2964de9453058022
Florian Bieringer <florian.bieringer@uni-passau.de>
2015-07-10 10:50:32 +0200

    cleanup plugin

--- WtfPlugin.class.php
+++ WtfPlugin.class.php
@@ -1,7 +1,5 @@
 <?php
 
-require 'bootstrap.php';
-
 /**
  * WtfPlugin.class.php
  *
@@ -14,39 +12,9 @@ class WtfPlugin extends StudIPPlugin implements SystemPlugin {
 
     public function __construct() {
         parent::__construct();
-
-        $navigation = new AutoNavigation(_('WTF'));
-        $navigation->setURL(PluginEngine::GetURL($this, array(), 'show'));
-        $navigation->setImage(Assets::image_path('blank.gif'));
-        Navigation::addItem('/wtfplugin', $navigation);
-
         PageLayout::addScript($this->getPluginURL() . '/assets/wysihtml5x-toolbar.min.js');
         PageLayout::addScript($this->getPluginURL() . '/assets/advanced_and_extended.js');
         PageLayout::addScript($this->getPluginURL() . '/assets/application.js');
         self::addStylesheet('assets/style.less');
     }
-
-    public function initialize() {
-        
-    }
-
-    public function perform($unconsumed_path) {
-        $this->setupAutoload();
-        $dispatcher = new Trails_Dispatcher(
-                $this->getPluginPath(), rtrim(PluginEngine::getLink($this, array(), null), '/'), 'show'
-        );
-        $dispatcher->plugin = $this;
-        $dispatcher->dispatch($unconsumed_path);
-    }
-
-    private function setupAutoload() {
-        if (class_exists('StudipAutoloader')) {
-            StudipAutoloader::addAutoloadPath(__DIR__ . '/models');
-        } else {
-            spl_autoload_register(function ($class) {
-                include_once __DIR__ . $class . '.php';
-            });
-        }
-    }
-
 }
--- views/show/index.php
+++
@@ -1,8 +0,0 @@
-<textarea cols="50" rows="10">Hallo **Studip** Community</textarea>
-
-<div id="wtf-toolbar">
-  <a data-wysihtml5-command="bold">bold</a>
-  <a data-wysihtml5-command="italic">italic</a>
-  <a data-wysihtml5-command="underline">underlined</a>
-  <a data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h1">H1</a>
-</div>
