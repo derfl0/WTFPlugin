f61a5e23cfb8bd6fe530f360b7a70b1c7e06dc03
Florian Bieringer <florian.bieringer@uni-passau.de>
2015-07-11 12:32:51 +0200

    liste

--- assets/application.js
+++ assets/application.js
@@ -2,19 +2,24 @@ STUDIP.wtf = {
     id: 1,
     replacements: [
         [/<br>/ig, '\n'], //newline
+        [/&nbsp;/ig, ' '], //space
         [/<h.>\s*(<img.*?>)\s*<\/h.>/img, '$1 '], //image header fix
         [/<span.*?>(.*?)<\/span>/img, '$1'], //spanfix
         [/<img.*?src=["|'](.*?)["|'].*?>/ig, '[img]$1 '], //image
-        [/<b>(.*?)<\/b>/gm, '**$1**'], //bold
-        [/<u>(.*?)<\/u>/gm, '__$1__'], //underlined
-        [/<i>(.*?)<\/i>/gm, '%%$1%%'], //italic
+        [/<b>([^]*?)<\/b>/gm, '**$1**'], //bold
+        [/<u>([^]*?)<\/u>/gm, '__$1__'], //underlined
+        [/<i>([^]*?)<\/i>/gm, '%%$1%%'], //italic
         [/(?:\n?)<h1>((?:.|\n)*?)<\/h1>/g, '\n!!!!$1\n'], //header1
         [/(?:\n?)<h2>((?:.|\n)*?)<\/h2>/g, '\n!!!$1\n'], //header2
         [/(?:\n?)<h3>((?:.|\n)*?)<\/h3>/g, '\n!!$1\n'], //header3
-        [/(?:\n?)<h4>((?:.|\n)*?)<\/h4>/g, '\n!$1\n'], //header4  
+        [/(?:\n?)<h4>((?:.|\n)*?)<\/h4>/g, '\n!$1\n'], //header4
+        [/<li>(.*?)<\/li>/gi, '- $1\n'], //list  
+        [/<ul>([\s|\S]*?)<\/ul>/gi, '$1'], //list  
 
     ],
     forward: [
+        [/- (.*)/ig, "<ul><li>$1</li></ul>"],
+        [/<\/ul>\s*<ul>/ig, ""],
         [/\*\*(.*?)\*\*/g, '<b>$1</b>'],
         [/__(.*?)__/g, '<u>$1</u>'],
         [/%%(.*?)%%/g, '<i>$1</i>'],
@@ -36,7 +41,7 @@ STUDIP.wtf = {
         });
         return input;
     },
-    toolbarAdd: function(toolbar, command, text, block) {
+    toolbarAdd: function (toolbar, command, text, block) {
         var button = $('<div>', {'data-wysihtml5-command': command, html: text});
         if (command === "formatBlock") {
             button.attr('data-wysihtml5-command-value', block);
@@ -59,7 +64,7 @@ $(document).ready(function () {
             textarea.attr("data-wtf", wtfid);
             textarea.addClass('wtf');
             var text = STUDIP.wtf.toRealHtml(textarea.val());
-            var wtf = $('<div>', {id: wtfid, class: 'wtf', contenteditable: 'true', height: textarea.height(), html:text });
+            var wtf = $('<div>', {id: wtfid, class: 'wtf', contenteditable: 'true', height: textarea.height(), html: text});
             textarea.after(wtf);
             wtf.css('width', textarea.css('width') - 2);
             wtf.height(textarea.css('height') - 2);
@@ -69,22 +74,24 @@ $(document).ready(function () {
             STUDIP.wtf.toolbarAdd(toolbar, 'bold', "<b>b</b>");
             STUDIP.wtf.toolbarAdd(toolbar, 'italic', "<i>i</i>");
             STUDIP.wtf.toolbarAdd(toolbar, 'underline', "<u>u</u>");
+            STUDIP.wtf.toolbarAdd(toolbar, 'insertUnorderedList', 'Liste');
             STUDIP.wtf.toolbarAdd(toolbar, 'formatBlock', "Ü1", "h1");
-            toolbar.append($('<div>', {id: 'swap-' + wtfid, text: "WYSIWYG"}));
+            toolbar.append($('<div>', {id: 'swap-' + wtfid, class: 'wtf-swap active', text: "WYSIWYG"}));
             textarea.before(toolbar);
 
             // Bind swapper
             $('#swap-' + wtfid).click(function (event) {
                 event.preventDefault();
                 textarea.toggle();
+                $(this).toggleClass('active');
                 wtf.toggle();
             });
-            
+
             // typing in textarea
-             textarea.keyup(function () {
-                 wtf.html(STUDIP.wtf.toRealHtml(textarea.val()));
-             });
-            
+            textarea.keyup(function () {
+                wtf.html(STUDIP.wtf.toRealHtml(textarea.val()));
+            });
+
             // typing in wtf
             wtf.keyup(function (event) {
                 wtf.find('*:not(br):empty').remove();
